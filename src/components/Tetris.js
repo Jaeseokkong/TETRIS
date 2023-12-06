1dd26c23de42e95376eb6edd55f16ef48681ce56
JAESEOK <wotjr5124@gmail.com>
2023-12-06 17:26:52 +0900

    스크린 스타일 적용중

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createScreen } from '../gameHelper';
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 //Components
 import Stage from './Stage';
@@ -9,15 +10,19 @@ import StartButton from './StartButton';
 const Tetris = () => {
   return (
     <div>
-        <Stage stage={createScreen()}/>
-        <aside>
-            <div>
-                <Display text="점수"/>
-                <Display text="줄 수"/>
-                <Display text="레벨"/>
-            </div>
-            <StartButton/>
-        </aside>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createScreen()}/>
+                <aside>
+                    <div>
+                        <Display text="점수"/>
+                        <Display text="줄 수"/>
+                        <Display text="레벨"/>
+                    </div>
+                    <StartButton/>
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     </div>
   )
 }
