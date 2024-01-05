6eb42c6a05a0a09e509663e12cc3a4ecccf7c9ea
Jaeseokkong <wotjr5124@gmail.com>
2024-01-05 11:33:41 +0900

    O tetrominos color 수정

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -35,7 +35,7 @@ export const TETROMINOS = {
                     ['O', 'O'],
                     ['O', 'O'],
                 ],
-        color: '0, 240, 240',
+        color: '240, 240, 0',
         type: 'O'
     },
     S: {
